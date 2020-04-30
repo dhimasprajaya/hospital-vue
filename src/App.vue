24f446c19b53fe1a5e15df9fb4a5c02dc1759ef1
Dhimas Prajaya <dhimasprajaya@gmail.com>
2020-05-01 04:01:04 +0700

    create application bar and navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,95 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <!-- Navigation Drawer -->
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense nav>
+        <v-list-item-group v-model="item" color="primary">
+          <div v-for="item in items" :key="item.title">
+            <!-- If Menu contains Sub Menu -->
+            <v-list-group v-if="item.items" :prepend-icon="item.icon">
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.title"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+
+              <v-list-item v-for="subItem in item.items" :key="subItem.title">
+                <v-list-item-icon>
+                  <v-icon v-text="subItem.icon"></v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="subItem.title"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-group>
+
+            <!-- If Menu don't contains Sub Menu -->
+            <v-list-item v-else>
+              <v-list-item-icon>
+                <v-icon v-text="item.icon"></v-icon>
+              </v-list-item-icon>
+              <v-list-item-content>
+                <v-list-item-title v-text="item.title"></v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <!-- Application Bar -->
+    <v-app-bar app color="primary" dark clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Hospital Apps</v-toolbar-title>
+    </v-app-bar>
+
+    <!-- Content -->
+    <v-content>
+      <v-container class="fill-height" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    drawer: null,
+    item: null,
+    items: [
+      { title: "Dashboard", icon: "mdi-view-dashboard", link: "" },
+      {
+        title: "Master",
+        icon: "mdi-database",
+        items: [
+          { title: "Hospital", icon: "mdi-hospital-building", link: "" },
+          { title: "Doctor", icon: "mdi-account", link: "" },
+          { title: "Patient", icon: "mdi-account-box", link: "" },
+        ],
+      },
+      { title: "Setting", icon: "mdi-wrench" },
+      { title: "Logout", icon: "mdi-exit-to-app" },
+    ],
+  }),
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.b-red {
+  border: 1px solid red;
 }
 
-#nav {
-  padding: 30px;
+.b-green {
+  border: 1px solid green;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.b-blue {
+  border: 1px solid blue;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.center {
+  margin: auto;
 }
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,22 +1,22 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib';
+import Vue from "vue";
+import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
-      options: {
-        customProperties: true,
-      },
+    options: {
+      customProperties: true,
+    },
     themes: {
       light: {
-        primary: '#ee44aa',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
+        primary: "#1B33BF",
+        secondary: "#364ABF",
+        accent: "#BF9663",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
       },
     },
   },
